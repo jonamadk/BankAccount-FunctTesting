4a7a6e7efc96356c55901c070dc54ed6ed460b59
Manoj Adhikari <ADHIKARIM@ETSU.EDU>
2024-09-29 00:03:52 -0400

    Deposit Amount

--- BankAccountTesting/ABankAccount.cs
+++ BankAccountTesting/ABankAccount.cs
@@ -2,11 +2,14 @@ using BankAccountLib;
 using NUnit.Framework.Internal;
 using System.Runtime.ConstrainedExecution;
 using System.Security.Principal;
+using System.Text;
 
 namespace BankAccountTesting
 {
     public class ABankAccount
     {
+
+        
 
 
         [TestCase("0abc123def6789", 100, TestName = "ShouldSetTheAccountNumberAndTheBalanceWhenConstructed")]
@@ -24,7 +27,8 @@ namespace BankAccountTesting
         [TestCase("0abc123def6789", -200, TestName = "ShouldThrowArgumentExceptionWhenConstructedForConstructedWithLessThanZero")]
         [TestCase("", 500, TestName = "ShouldThrowArgumentExceptionWhenConstructedForConstructedWithWhiteSpace")]
         [TestCase("", -500, TestName = "ShouldThrowArgumentExceptionWhenConstructedForWhiteSpaceAndBalanceLessthanZero")]
-        public void ShouldThrowArgumentExceptionWhenAccountNumberAndBalanceIsConstructed(string accountNumber, decimal initialBalance) {
+        public void ShouldThrowArgumentExceptionWhenAccountNumberAndBalanceIsConstructed(string accountNumber, decimal initialBalance)
+        {
 
             Assert.Throws<ArgumentException>(() => new BankAccount(accountNumber, initialBalance));
         }
@@ -36,7 +40,7 @@ namespace BankAccountTesting
 
             string accountNumber = "0abc123def6789";
             decimal initialBalance = 10;
-            var sut = new BankAccount( accountNumber, initialBalance);
+            var sut = new BankAccount(accountNumber, initialBalance);
 
             sut.Deposit(amount);
 
@@ -58,7 +62,7 @@ namespace BankAccountTesting
         }
 
 
-        [TestCase(10, TestName ="ShouldDecreaseBalanceAfterWithdraw")]
+        [TestCase(10, TestName = "ShouldDecreaseBalanceAfterWithdraw")]
         public void ShouldDecreaseBalanceAfterWithdraw(decimal amount)
         {
             string accountNumber = "0abc123def6789";
@@ -71,15 +75,15 @@ namespace BankAccountTesting
 
         }
 
-     
-        [TestCase(100,-20, TestName = "ShouldThrowArgumentExceptionWhenWithdrawAmountIsNegative")]
-        [TestCase(100,0, TestName = "ShouldThrowArgumentExceptionWhenWithdrawAmountIsZero")]
-       
-        public void ShouldThrowArgumentExceptionWhenWithdrawAmountLessThanEqualToZero( decimal initialBalance, decimal amount)
+
+        [TestCase(100, -20, TestName = "ShouldThrowArgumentExceptionWhenWithdrawAmountIsNegative")]
+        [TestCase(100, 0, TestName = "ShouldThrowArgumentExceptionWhenWithdrawAmountIsZero")]
+
+        public void ShouldThrowArgumentExceptionWhenWithdrawAmountLessThanEqualToZero(decimal initialBalance, decimal amount)
         {
 
             string accountNumber = "0abc123def6789";
-     
+
             var sut = new BankAccount(accountNumber, initialBalance);
 
 
@@ -98,7 +102,7 @@ namespace BankAccountTesting
         }
 
 
-        [TestCase(50, "Low",  TestName = "ShouldReturnAccountStatusAsLow")]
+        [TestCase(50, "Low", TestName = "ShouldReturnAccountStatusAsLow")]
         [TestCase(500, "Normal", TestName = "ShouldReturnAccountStatusAsNormal")]
         [TestCase(1500, "High", TestName = "ShouldReturnAccountStatusAsHigh")]
 
@@ -106,7 +110,7 @@ namespace BankAccountTesting
         public void ShouldReturnAccountStatusLowNormalorHighBasedOnBalance(decimal initialBalance, string accountStatus)
         {
             string accountNumber = "0abc123def6789";
-          
+
             var sut = new BankAccount(accountNumber, initialBalance);
 
             string accountStatusIs = sut.GetAccountStatus();
@@ -128,12 +132,11 @@ namespace BankAccountTesting
 
 
             var recipient = new BankAccount(recpAccountNumber, recpInitialBalance);
-
             var sut = new BankAccount(accountNumber, initialBalance);
 
             sut.TransferTo(recipient, amount);
 
-            
+
             Assert.That(recipient.Balance, Is.EqualTo(200));
             Assert.That(sut.Balance, Is.EqualTo(400));
         }
@@ -150,10 +153,9 @@ namespace BankAccountTesting
 
 
             var recipient = new BankAccount(recpAccountNumber, recpInitialBalance);
-
             var sut = new BankAccount(accountNumber, initialBalance);
 
-           
+
 
             Assert.Throws<InvalidOperationException>(() => sut.TransferTo(recipient, amount));
 
@@ -161,7 +163,7 @@ namespace BankAccountTesting
         }
 
         [TestCase(300, TestName = "ShouldThrowInvalidOperationExceptionWhenTransferAmountGreaterThanBalanceForPositiveAmount")]
-        
+
         public void ShouldThrowInvalidOperationExceptionForInsufficientFund(decimal amount)
         {
             string recpAccountNumber = "recp123def6789";
@@ -172,7 +174,6 @@ namespace BankAccountTesting
 
 
             var recipient = new BankAccount(recpAccountNumber, recpInitialBalance);
-
             var sut = new BankAccount(accountNumber, initialBalance);
 
 
@@ -195,9 +196,7 @@ namespace BankAccountTesting
 
 
             var recipient = new BankAccount(recpAccountNumber, recpInitialBalance);
-
             var sut = new BankAccount(accountNumber, initialBalance);
-
 
 
             Assert.Throws<ArgumentException>(() => sut.TransferTo(recipient, amount));
@@ -207,8 +206,134 @@ namespace BankAccountTesting
 
 
 
+        // 2 BOUNDARY VALUE ANALYSIS //
+        [TestCase("", 500, TestName = "Constructor_EmptyAccountNumber")]
+        [TestCase("123456789", -1, TestName = "Constructor_NegativeBalance")]
+        [TestCase("123456789", 0, TestName = "Constructor_ZeroBalance")]
+        [TestCase("123456789", 1, TestName = "Constructor_PositiveBalance")]
+        public void EvalauteConstructor_BoundaryValueAnalysis(string accountNumber, decimal initialBalance)
+        {
+
+            if (accountNumber == "" || initialBalance < 0)
+            {
+
+                Assert.Throws<ArgumentException>(() => new BankAccount(accountNumber, initialBalance));
+            }
+
+            else
+            {
+                var sut = new BankAccount(accountNumber, initialBalance);
+                Assert.That(sut.AccountNumber, Is.EqualTo(accountNumber));
+                Assert.That(sut.Balance, Is.EqualTo(initialBalance));
+            }
+
+        }
+
+
+        [TestCase("123456789", 1e-28, TestName = "VerySmallPositivevalueBoundary")]
+        [TestCase("123456789", 1e-27, TestName = "JustBelowVerySmallPositivevalueBoundary")]
+      
+
+        public void EvaluateConstructor_VerySmallPositiveValue(string accountNumber, decimal initialBalance)
+        {
+
+           
+                var sut = new BankAccount(accountNumber, initialBalance);
+                Assert.That(sut.AccountNumber, Is.EqualTo(accountNumber));
+                Assert.That(sut.Balance, Is.EqualTo(initialBalance));
+
+        }
+
+        [TestCase("123456789", -1e-28, TestName = "VerySmallPositivevalueBoundary")]
+        [TestCase("123456789", -1e-27, TestName = "JustBelowVerySmallPositivevalueBoundary")]
+        public void EvaluateConstructor_VerySmallNegativeValue(string accountNumber, decimal initialBalance)
+        {
+
+
+            Assert.Throws<ArgumentException>(
+                    () => new BankAccount(accountNumber, initialBalance));
+        }
+
+
+        [TestCase()]
+        public void EvaluateConstructor_VerylargePositiveValue()
+        {
+
+            string accountNumber = "123456789";
+            decimal initialBalance = decimal.MaxValue;  
+
+            var sut = new BankAccount(accountNumber, initialBalance);
+            Assert.That(sut.AccountNumber, Is.EqualTo(accountNumber));
+            Assert.That(sut.Balance, Is.EqualTo(initialBalance));
+
+
+
+
+        }
+        [Test]
+        public void EvaluateConstructor_VeryLargeNegativePositiveValue()
+        {
+
+            string accountNumber = "123456789";
+            decimal initialBalance = decimal.MinValue;
+
+
+            Assert.Throws<ArgumentException>(
+               () => new BankAccount(accountNumber, initialBalance));
+
+
+        }
+
+        // Deposit Method
+       
+        [TestCase(0, TestName = "DepositZero")]
+        [TestCase(-0.01, TestName = "DepositJustBelowZero")]
+        [TestCase(0.01, TestName = "DepositJustAboveZero")]
+        public void EvalaueDeposit_BoundaryValueAnalysis(decimal amount)
+        {
+            string accountNumber = "0abc456789";
+            decimal initialBalance = 10;
+
+            var sut = new BankAccount(accountNumber, initialBalance);
+            if (amount <= 0)
+            {
+                Assert.Throws<ArgumentException>(
+                        () => sut.Deposit(amount));
+            }
+            else
+            {
+        
+                sut.Deposit(amount);
+                Assert.That(sut.Balance, Is.EqualTo(initialBalance + amount));
+            }
+        }
+
+        [Test]
+        public void Deposit_VeryLargePositiveValue()
+
+        {
+            string accountNumber = "0abc456789";
+            decimal initialBalance = 100;
+            decimal amount = decimal.MaxValue;
+
+            var sut = new BankAccount(accountNumber, initialBalance);
+            sut.Deposit(amount);
+            Assert.That(sut.Balance, Is.EqualTo(initialBalance + amount));
+        }
+
+        [Test]
+        public void Deposit_VeryLargeNegativeValue()
+        {
+            string accountNumber = "0abc456789";
+            decimal initialBalance = 100;
+            decimal amount = decimal.MinValue;
+
+            var sut = new BankAccount(accountNumber, initialBalance);
+            Assert.Throws<ArgumentException>(
+                        () => sut.Deposit(amount));
+        }
+
 
 
     }
-
 }
